c0d3ac00a7bee1eb10e30e8bb33292877962dc64
Jorge Miguel Perugorria <jorsss.v7.1@gmail.com>
2021-03-16 21:55:53 -0300

    Selector para la seccion de noticias

--- src/main.js
+++ src/main.js
@@ -2,16 +2,25 @@ let host = 'https://migham.github.io';
 let Cabecera, HeaderURL   = `${host}/hm/cabecera.html`;
 let Noticias, NoticiasURL = `${host}/hm/noticias.html`;
 let Guias,    GuiasURL    = `${host}/hm/guias.html`;
+let MainPrincipal;
 
 function Main() {
 	Cabecera = document.getElementById('cabecera');
 	Noticias = document.getElementById('noticias');
 	Guias = document.getElementById('guias');
+	MainPrincipal = document.getElementById('main');
+
+	if(MainPrincipal.classList.contains('guia-html'))
+	{
+		CargarElemento(Noticias, `${host}/hm/items-html.html`);
+	}
 
 	CargarElemento(Cabecera, HeaderURL);
 }
 
 window.addEventListener('load', Main, false);
+
+
 
 /**
  * Funcion que permite agregar elementos al documento
